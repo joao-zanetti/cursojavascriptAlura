1d567e001014d6d1717a1700714b74bc2a51b8cc
joao-zanetti <57362590+joao-zanetti@users.noreply.github.com>
2019-11-10 16:54:08 -0300

    Update percorrePTr.js

--- percorrePTr.js
+++ percorrePTr.js
@@ -1,4 +1,7 @@
-
+/*
+    Developed by João Zanetti
+    https://github.com/joao-zanetti
+*/
 function percorreArrayPTr(trsPacientes, comportamento){  //recebe um array de  tr da classe paciente  e uma funcao comportamento
 
 	for(posicaoAtual=0;posicaoAtual<=trsPacientes.length -1 ; posicaoAtual++){  //percorre todas posicoes do vetor de trs
